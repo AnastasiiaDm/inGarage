20fe6e51c0c6a197bc6d5ec12c4698a24bc821d2
AnastasiiaDM <tt266744@gmail.com>
2018-04-11 17:12:20 +0300

    nextPage

--- src/test/java/FilterCheck.java
+++ src/test/java/FilterCheck.java
@@ -1,11 +1,15 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
+
 
 import java.util.List;
 import java.util.regex.Matcher;
@@ -32,21 +36,57 @@ public class FilterCheck {
         getCDChangerCheck.click();
         Thread.sleep(2000);
 
-        WebElement filterResult = browser.findElement(By.cssSelector("div[class='rows']"));
+        WebElement filterResult = browser.findElement(By.cssSelector("div.rows"));
         String result = filterResult.getText();
+        List<WebElement> resultCount = browser.findElements(By.cssSelector("div.product_item.ajax-item"));
 
 
 
+        WebElement nextPage = browser.findElement(By.cssSelector(".pagination .pagination li:last-of-type a"));
+        nextPage.click();
+        Thread.sleep(5000);
+        List<WebElement> resultCountnextPage = browser.findElements(By.cssSelector("div.product_item.ajax-item"));
 
 
-        List<WebElement> resultCount = browser.findElements(By.cssSelector("div[class='product_item ajax-item']"));
-
-
-
-        System.out.println(resultCount.size());
+        System.out.println(resultCount.size() + resultCountnextPage.size());
 
 //        Assert.assertTrue();
 
 
     }
+
+//    public boolean waitForJSandJQueryToLoad() {
+//
+//        WebDriverWait wait = new WebDriverWait(browser, 30);
+//
+//        // wait for jQuery to load
+//        ExpectedCondition<Boolean> jQueryLoad = new ExpectedCondition<Boolean>() {
+//            @Override
+//            public Boolean apply(WebDriver driver) {
+//                try {
+//                    return ((Long)((JavascriptExecutor)driver).executeScript("return jQuery.active") == 0);
+//                }
+//                catch (Exception e) {
+//                    // no jQuery present
+//                    return true;
+//                }
+//            }
+//        };
+//
+//        // wait for Javascript to load
+//        ExpectedCondition<Boolean> jsLoad = new ExpectedCondition<Boolean>() {
+//
+//            @Override
+//            public Boolean apply(WebDriver driver) {
+//                return ((JavascriptExecutor)driver).executeScript("return document.readyState")
+//                        .toString().equals("complete");
+//            }
+//        };
+//
+//        return wait.until(jQueryLoad) && wait.until(jsLoad);
+//    }
+
+
+
+
 }
