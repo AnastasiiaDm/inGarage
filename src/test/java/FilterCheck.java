bd304f1edebb12fda5214f20b911e242912fb3ed
AnastasiiaDM <tt266744@gmail.com>
2018-04-13 19:07:34 +0300

    check existing of pagination; scroll to the top; Webasto test

--- src/test/java/FilterCheck.java
+++ src/test/java/FilterCheck.java
@@ -1,12 +1,11 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import java.util.List;
+import org.openqa.selenium.JavascriptExecutor;
 
 
 public class FilterCheck {
@@ -27,38 +26,89 @@ public class FilterCheck {
     }
 
     @Test
-    public void filterCheck() throws InterruptedException, IllegalArgumentException {
+    public void cDChanger() throws InterruptedException, IllegalArgumentException {
 
         JSWaiter jsWaiter = new JSWaiter(browser);
 
         WebElement getCDChangerCheck = browser.findElement(By.cssSelector("input[value='CD-чейнджер'] + label"));
         getCDChangerCheck.click();
-
         while (!jsWaiter.waitForJQueryLoad()) {
             Thread.sleep(100);
         }
-//        Thread.sleep(2000);
+
         WebElement filterResultFirstPage = browser.findElement(By.cssSelector("div.rows"));
         String firstPageResult = filterResultFirstPage.getText();
-        List<WebElement> resultCount = browser.findElements(By.cssSelector("div.product_item.ajax-item"));
+        List<WebElement> countFirstPage = browser.findElements(By.cssSelector("div.product_item.ajax-item"));
+
 
         WebElement nextPage = browser.findElement(By.cssSelector(".pagination .pagination li:last-of-type a"));
-        nextPage.click();
+        if (nextPage != null) {
 
+            nextPage.click();
+            while (!jsWaiter.waitForJQueryLoad()) {
+                Thread.sleep(100);
+            }
+            List<WebElement> countNextPage = browser.findElements(By.cssSelector("div.product_item.ajax-item"));
+
+            WebElement readResultFirstPage = browser.findElement(By.cssSelector("div.rows"));
+            String resultNextPage = readResultFirstPage.getText();
+
+            System.out.println("\n" + "Test 1: filter result for CD-changer:" + "\n" + firstPageResult + "\n" + resultNextPage + "\n (" + (countFirstPage.size() + countNextPage.size()) + ")");
+        } else {
+            System.out.println("\n" + "Test 1: filter result for CDChanger:" + "\n" + firstPageResult + "\n (" + (countFirstPage.size() + ")"));
+        }
+
+        JavascriptExecutor js = (JavascriptExecutor)browser;
+        js.executeScript("window.scrollTo(0,0)");
+
+        getCDChangerCheck.click();
         while (!jsWaiter.waitForJQueryLoad()) {
             Thread.sleep(100);
         }
-        List<WebElement> listNextPage = browser.findElements(By.cssSelector("div.product_item.ajax-item"));
-
-        WebElement readResultFirstPage = browser.findElement(By.cssSelector("div.rows"));
-        String resultNextPage = readResultFirstPage.getText();
-
-
-        System.out.println(firstPageResult + "\n" + resultNextPage +  "\n (" + (resultCount.size() +  listNextPage.size()) + ")");
-
-//        Assert.assertTrue();
-
-
     }
 
+    @Test
+    public void webasto() throws InterruptedException, IllegalArgumentException {
+
+        JSWaiter jsWaiter = new JSWaiter(browser);
+
+        WebElement getWebasto = browser.findElement(By.cssSelector("input[value='Webasto (Отопитель автономный)'] + label"));
+        getWebasto.click();
+        while (!jsWaiter.waitForJQueryLoad()) {
+            Thread.sleep(100);
+        }
+
+        WebElement filterResultFirstPage = browser.findElement(By.cssSelector("div.rows"));
+        String firstPageResult = filterResultFirstPage.getText();
+        List<WebElement> countFirstPage = browser.findElements(By.cssSelector("div.product_item.ajax-item"));
+        WebElement nextPage = null;
+        try {
+            nextPage = browser.findElement(By.cssSelector(".pagination .pagination li:last-of-type a"));
+        } catch (NoSuchElementException ref) {
+            System.out.println(ref.getMessage());
+        }
+        if (nextPage != null) {
+
+            nextPage.click();
+            while (!jsWaiter.waitForJQueryLoad()) {
+                Thread.sleep(100);
+            }
+            List<WebElement> countNextPage = browser.findElements(By.cssSelector("div.product_item.ajax-item"));
+
+            WebElement readResultFirstPage = browser.findElement(By.cssSelector("div.rows"));
+            String resultNextPage = readResultFirstPage.getText();
+
+            System.out.println("\n" + "Test 2: filter result for Webasto:" + "\n" + firstPageResult + "\n" + resultNextPage + "\n (" + (countFirstPage.size() + countNextPage.size()) + ")");
+        } else {
+            System.out.println("\n" + "Test 2: filter result for Webasto:" + "\n" + firstPageResult + "\n (" + (countFirstPage.size() + ")"));
+        }
+//        Assert.assertTrue("Test 1 Passed" );
+        JavascriptExecutor js = (JavascriptExecutor) browser;
+        js.executeScript("window.scrollTo(0,0)");
+
+        getWebasto.click();
+        while (!jsWaiter.waitForJQueryLoad()) {
+            Thread.sleep(100);
+        }
+    }
 }
