11b57bc9dfdd5f2e5cd9afddd7de2a7ef0700414
AnastasiiaDm <anastasia8sergev@gmail.com>
2018-06-08 17:35:19 +0300

    hashMap fixed

--- src/main/java/TestSearchHelper.java
+++ src/main/java/TestSearchHelper.java
@@ -13,10 +13,12 @@ public class TestSearchHelper {
     private final long TIME_SLEEP = 500;
     private WebDriver browser;
     private JSWaiter jsWaiter;
+    HashMap<String, String> hashMapValue = new HashMap<String, String>();
 
     public TestSearchHelper(WebDriver browser) {
         this.browser = browser;
         jsWaiter = new JSWaiter(browser);
+
     }
 
 
@@ -32,21 +34,22 @@ public class TestSearchHelper {
             val = item.getText();
 
             if (TextUtils.isEmpty(val)) {
-//                System.out.println("no such element value");
+                System.out.println("no such element value");
+//                нашел значение
             } else {
-//  make massive
-                hashMapOfValues(browser, val);
-
-
                 System.out.println("val = " + val);
                 browser.findElement(By.cssSelector(".close")).click();
                 Thread.sleep(TIME_SLEEP);
-                break;
+                if (!hashMapValue.containsKey(val)){
+                    System.out.println("new value found");
+                    hashMapValue.put(val, ".origin_number");
+                    break;
+                }
+
             }
             browser.findElement(By.cssSelector(".close")).click();
             Thread.sleep(TIME_SLEEP);
         }
-
 
         js.executeScript("window.scrollTo(0, -document.body.scrollHeight);");
         browser.findElement(By.cssSelector("[placeholder='Введите слово']")).sendKeys(val);
@@ -56,19 +59,21 @@ public class TestSearchHelper {
 
         testSearchResult(browser, val);
 
+        Thread.sleep(TIME_SLEEP);
 
 
         while (browser.findElements(By.cssSelector(".pagination .pagination li:last-of-type a")).size() > 0 && browser.findElement(By.cssSelector(".pagination .pagination li:last-of-type a")).isEnabled()) {
-
             Thread.sleep(TIME_SLEEP);
             browser.findElement(By.cssSelector(".pagination .pagination li:last-of-type a")).click();
             LoaderWaiter.waitForLoad(browser);
 
             js.executeScript("window.scrollTo(0, -document.body.scrollHeight);");
-
             Thread.sleep(TIME_SLEEP);
+
             testSearchResult(browser, val);
         }
+        //        browser.findElement(By.cssSelector(".header_block [href='poisk.html']")).click();
+
     }
 
 //        проверить цифру из фильтра например марки , закинуть назввание марки в поиск и проверить количество наименований марки  в соответствии цифре из фильтра
@@ -90,17 +95,6 @@ public class TestSearchHelper {
             }
             browser.findElement(By.cssSelector(".close")).click();
         }
-    }
-
-
-    private void hashMapOfValues (WebDriver browser, String val){
-
-        HashMap<String, String> hashMap = new HashMap<String, String>();
-//        hashMap.containsKey(val);
-        hashMap.put(val, " .origin_number");
-        System.out.println(hashMap);
-
-
     }
 
 }
